7f74138191205a6a40c62debf8c22061ecee703e
En-Hsu Yen <a061105@gmail.com>
2016-08-02 13:18:42 -0500

    run_convex edit

--- LPconvert/run_convex.sh
+++ LPconvert/run_convex.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-train_data=../SimulatedData/train_doc.40.noise
-test_data=../SimulatedData/test_doc.40.noise
+train_data=../SimulatedData/docs
+test_data=../SimulatedData/docs
 #train_data=../RealData/ChineseOpinion/parsed_docs/state-3.op-as-pos.train
 #test_data=../RealData/ChineseOpinion/parsed_docs/state-3.op-as-pos.test
 
 
-#./LPconvert $train_data 10 0
+./LPconvert $train_data 10 0
 ../LPsparse/LPsparse -e 0.01 -t 0.005 .
-./paste.sh
-./parseToGivenH var_sol
+#./paste.sh
+#./parseToGivenH var_sol
 
-../LatentSVMtrain/latentTrain $train_data 10.0 1 0 var_sol.given_h
-../LatentSVMtrain/predict $test_data model
+#../LatentSVMtrain/latentTrain $train_data 10.0 1 0 var_sol.given_h
+#../LatentSVMtrain/predict $test_data model
