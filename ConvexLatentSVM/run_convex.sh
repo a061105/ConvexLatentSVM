b6300395be7aab6cb4d67e400b64af1222d90c1e
En-Hsu Yen <a061105@gmail.com>
2016-08-28 22:27:24 -0500

    C>=10

--- ConvexLatentSVM/run_convex.sh
+++ ConvexLatentSVM/run_convex.sh
@@ -2,8 +2,10 @@
 
 train_data=../SimulatedData/docs.train
 test_data=../SimulatedData/docs.test
+#train_data=../../docs.train
+#test_data=../../docs.test
 
 
 ./convexTrain $train_data 10 0.01 0
-../LatentSVMtrain/latentTrain $train_data 1.0 1 0 1.0 beta_assign
+../LatentSVMtrain/latentTrain $train_data 10.0 1 0 1.0 beta_assign
 ../LatentSVMtrain/predict $test_data model
